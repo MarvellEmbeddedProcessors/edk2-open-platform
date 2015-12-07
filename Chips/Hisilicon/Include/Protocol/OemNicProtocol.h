68ef6a86f8b713460ebd57c2a0904a053bad98c9
Heyi Guo <heyi.guo@linaro.org>
2015-12-07 10:39:30 +0000

    D02: Add interface to OemNicProtocol
    Signed-off-by: Heyi Guo <heyi.guo@linaro.org>

--- Chips/Hisilicon/Include/Protocol/OemNicProtocol.h
+++ Chips/Hisilicon/Include/Protocol/OemNicProtocol.h
@@ -4,6 +4,9 @@
 #define OEM_NIC_PROTOCOL_GUID   \
     { 0xb5903955, 0x31e9, 0x4aaf, { 0xb2, 0x83, 0x7, 0x9f, 0x3c, 0xc4, 0x71, 0x66 } }
 
+#define OEM_XGE_STATUS_PROTOCOL_GUID   \
+        { 0xa6b8ed0e, 0xd8cc, 0x4853, { 0xaa, 0x39, 0x2c, 0x3e, 0xcd, 0x7c, 0xa5, 0x97 } }
+
 typedef
 EFI_STATUS
 (EFIAPI *OEM_NIC_GET_MAC_ADDRESS) (
@@ -23,7 +26,21 @@ typedef struct {
   OEM_NIC_SET_MAC_ADDRESS SetMac;
 } OEM_NIC_PROTOCOL;
 
+typedef
+VOID
+(*OEM_FEEDBACK_XGE_STATUS) (
+  BOOLEAN IsLinkup,
+  BOOLEAN IsActOK,
+  UINT32 port
+  );
+
+typedef struct {
+  OEM_FEEDBACK_XGE_STATUS FeedbackXgeStatus;
+} OEM_XGE_STATUS_PROTOCOL;
+
+
 extern EFI_GUID gOemNicProtocolGuid;
+extern EFI_GUID gOemXgeStatusProtocolGuid;
 
 
 #endif
