84caf8aeb7490b554066d370b6b0dc671ff6efc3
Jan Dąbroś <jsd@semihalf.com>
2016-06-06 15:06:46 +0200

    Drivers/Sd: fix: Set slow mode for SD/eMMC
    BIT29 not BIT19 is responsible for setting SLOW_MODE.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Jan Dabros <jsd@semihalf.com>

--- Drivers/Sd/XenonSdhci.c
+++ Drivers/Sd/XenonSdhci.c
@@ -350,7 +350,7 @@ XenonSetPhy (
       (Timing == MMC_TIMING_UHS_SDR25)) {
     SdMmcHcRwMmio(PciIo, SD_BAR_INDEX, EMMC_PHY_TIMING_ADJUST, TRUE, 4, &Var);
 
-    Var |= OUTPUT_QSN_PHASE_SELECT | (1 << 19);
+    Var |= OUTPUT_QSN_PHASE_SELECT | (1 << 29);
     SdMmcHcRwMmio(PciIo, SD_BAR_INDEX, EMMC_PHY_TIMING_ADJUST, FALSE, 4, &Var);
   }
 
