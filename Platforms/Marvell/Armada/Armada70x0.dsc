Semihalf/uefi

Platforms/Marvell: Enable Pp2Dxe driver on Armada70x0 platform
Armada 7040 development board comprise three different ports:
eth0: SFP cage with fixed link to 1G
eth1: Optional 8-port switch with fixed link to 2.5G
eth2: 1G RGMII port with autonegotiation enabled

This change also required MPP setting modification.

Contributed-under: TianoCore Contribution Agreement 1.0
Signed-off-by: Bartosz Szczepanek <bsz@semihalf.com>
Signed-off-by: Marcin Wojtas <mw@semihalf.com>

Platforms/Marvell/Armada/Armada70x0.dsc
@@ -112,6 +112,22 @@
   gMarvellTokenSpaceGuid.PcdMdioBaseAddress|0xF212A200
 
   #PHY
-  gMarvellTokenSpaceGuid.PcdPhyConnectionTypes|{ 0x0, 0x0 }
+  gMarvellTokenSpaceGuid.PcdPhyConnectionTypes|{ 0x4, 0x4, 0x0 }
   gMarvellTokenSpaceGuid.PcdPhyDeviceIds|{ 0x0, 0x0 }
   gMarvellTokenSpaceGuid.PcdPhyStartupAutoneg|FALSE
+
+  #NET
+  gMarvellTokenSpaceGuid.PcdPhySmiAddresses|{ 0xff, 0x0, 0x1 }
+  gMarvellTokenSpaceGuid.PcdPp2PortNumber|3
+  gMarvellTokenSpaceGuid.PcdPp2PortIds|{ 0x0, 0x1, 0x2 }
+  gMarvellTokenSpaceGuid.PcdPp2GopIndexes|{ 0x0, 0x2, 0x3 }
+  gMarvellTokenSpaceGuid.PcdPp2InterfaceAlwaysUp|{ 0x1, 0x1, 0x0 }
+  gMarvellTokenSpaceGuid.PcdPp2InterfaceSpeed|{ 0x3, 0x4, 0x3 }
+  gMarvellTokenSpaceGuid.PcdPp2SharedAddress|0xf2000000
+  gMarvellTokenSpaceGuid.PcdPp2GmacBaseAddress|0xf2130e00
+  gMarvellTokenSpaceGuid.PcdPp2GmacObjSize|0x1000
+  gMarvellTokenSpaceGuid.PcdPp2XlgBaseAddress|0xf2130f00
+  gMarvellTokenSpaceGuid.PcdPp2XlgObjSize|0x1000
+  gMarvellTokenSpaceGuid.PcdPp2Rfu1BaseAddress|0xf2441000
+  gMarvellTokenSpaceGuid.PcdPp2SmiBaseAddress|0xf212A200
+  gMarvellTokenSpaceGuid.PcdPp2ClockFrequency|333333333

MarvellEmbeddedProcessors/edk2-open-platform

Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>

Platforms/Marvell/Armada/Armada70x0.dsc
@@ -119,9 +119,25 @@
   gMarvellTokenSpaceGuid.PcdMdioBaseAddress|0xF212A200
 
   #PHY
-  gMarvellTokenSpaceGuid.PcdPhyConnectionTypes|{ 0x0, 0x0 }
+  gMarvellTokenSpaceGuid.PcdPhyConnectionTypes|{ 0x4, 0x4, 0x0 }
   gMarvellTokenSpaceGuid.PcdPhyDeviceIds|{ 0x0, 0x0 }
   gMarvellTokenSpaceGuid.PcdPhyStartupAutoneg|FALSE
+
+  #NET
+  gMarvellTokenSpaceGuid.PcdPhySmiAddresses|{ 0xff, 0x0, 0x1 }
+  gMarvellTokenSpaceGuid.PcdPp2ClockFrequency|333333333
+  gMarvellTokenSpaceGuid.PcdPp2GmacBaseAddress|0xf2130e00
+  gMarvellTokenSpaceGuid.PcdPp2GmacDevSize|0x1000
+  gMarvellTokenSpaceGuid.PcdPp2GopIndexes|{ 0x0, 0x2, 0x3 }
+  gMarvellTokenSpaceGuid.PcdPp2InterfaceAlwaysUp|{ 0x1, 0x1, 0x0 }
+  gMarvellTokenSpaceGuid.PcdPp2InterfaceSpeed|{ 0x3, 0x4, 0x3 }
+  gMarvellTokenSpaceGuid.PcdPp2NumPorts|3
+  gMarvellTokenSpaceGuid.PcdPp2PortIds|{ 0x0, 0x1, 0x2 }
+  gMarvellTokenSpaceGuid.PcdPp2Rfu1BaseAddress|0xf2441000
+  gMarvellTokenSpaceGuid.PcdPp2SharedAddress|0xf2000000
+  gMarvellTokenSpaceGuid.PcdPp2SmiBaseAddress|0xf212A200
+  gMarvellTokenSpaceGuid.PcdPp2XlgBaseAddress|0xf2130f00
+  gMarvellTokenSpaceGuid.PcdPp2XlgDevSize|0x1000
 
   #ResetLib
   gMarvellTokenSpaceGuid.PcdResetRegAddress|0xf06f0084