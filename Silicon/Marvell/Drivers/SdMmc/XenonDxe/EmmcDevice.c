5cb3f669e2c60836794f4d54dbccba64b485d678
Joe Zhou <shjzhou@marvell.com>
2017-11-23 11:52:00 +0100

    Marvell/Drivers: XenonDxe: Adjust eMMC clock and bus width sequence
    HS mode for eMMC requires changing the bus width after
    clock configuration, so reflect this in EmmcSwitchToHighSpeed ()
    routine.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Joe Zhou <shjzhou@marvell.com>

--- Silicon/Marvell/Drivers/SdMmc/XenonDxe/EmmcDevice.c
+++ Silicon/Marvell/Drivers/SdMmc/XenonDxe/EmmcDevice.c
@@ -749,10 +749,6 @@ EmmcSwitchToHighSpeed (
 
   Private = SD_MMC_HC_PRIVATE_FROM_THIS (PassThru);
 
-  Status = EmmcSwitchBusWidth (PciIo, PassThru, Slot, Rca, IsDdr, BusWidth);
-  if (EFI_ERROR (Status)) {
-    return Status;
-  }
   //
   // Set to Hight Speed timing
   //
@@ -789,6 +785,11 @@ EmmcSwitchToHighSpeed (
 
   HsTiming = 1;
   Status = EmmcSwitchClockFreq (PciIo, PassThru, Slot, Rca, HsTiming, ClockFreq);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  Status = EmmcSwitchBusWidth (PciIo, PassThru, Slot, Rca, IsDdr, BusWidth);
   if (EFI_ERROR (Status)) {
     return Status;
   }
