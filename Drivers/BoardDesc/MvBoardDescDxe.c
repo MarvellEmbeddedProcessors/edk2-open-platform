56f3f3ba122076b1c2f862b2c2d5e023296d4902
Marcin Wojtas <mw@semihalf.com>
2018-03-29 12:22:11 +0200

    Marvell/Armada: Extend MvBoardDesc protocol with MDIO support
    Introduce new callback that can provide information
    about MDIO controllers to the Mdio driver.
    
    Extend ArmadaBoardDescLib with new structure MV_BOARD_MDIO_DESC,
    for holding board specific data.
    
    Change-Id: If131bb2221661eed6cd0d5ef1abcce25ddad556b
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Marcin Wojtas <mw@semihalf.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/52828
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Hua Jing <jinghua@marvell.com>

--- Drivers/BoardDesc/MvBoardDescDxe.c
+++ Drivers/BoardDesc/MvBoardDescDxe.c
@@ -110,6 +110,42 @@ MvBoardDescGpioGet (
 
 STATIC
 EFI_STATUS
+MvBoardDescMdioGet (
+  IN MARVELL_BOARD_DESC_PROTOCOL  *This,
+  IN OUT MV_BOARD_MDIO_DESC      **MdioDesc
+  )
+{
+  MV_BOARD_MDIO_DESC *BoardDesc;
+  MV_SOC_MDIO_DESC *SoCDesc;
+  EFI_STATUS Status;
+  UINT8 MdioCount;
+  UINTN Index;
+
+  /* Get SoC data about all available MDIO controllers */
+  Status = ArmadaSoCDescMdioGet (&SoCDesc, &MdioCount);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  /* Allocate and fill board description */
+  BoardDesc = AllocateZeroPool (MdioCount * sizeof (MV_BOARD_MDIO_DESC));
+  if (BoardDesc == NULL) {
+    DEBUG ((DEBUG_ERROR, "%a: Cannot allocate memory\n", __FUNCTION__));
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  for (Index = 0; Index < MdioCount; Index++) {
+    BoardDesc[Index].SoC = &SoCDesc[Index];
+  }
+
+  BoardDesc->MdioDevCount = MdioCount;
+  *MdioDesc = BoardDesc;
+
+  return EFI_SUCCESS;
+}
+
+STATIC
+EFI_STATUS
 MvBoardDescAhciGet (
   IN MARVELL_BOARD_DESC_PROTOCOL  *This,
   IN OUT MV_BOARD_AHCI_DESC      **AhciDesc
@@ -436,6 +472,7 @@ MvBoardDescInitProtocol (
 {
   BoardDescProtocol->BoardDescComPhyGet = MvBoardDescComPhyGet;
   BoardDescProtocol->BoardDescGpioGet = MvBoardDescGpioGet;
+  BoardDescProtocol->BoardDescMdioGet = MvBoardDescMdioGet;
   BoardDescProtocol->BoardDescAhciGet = MvBoardDescAhciGet;
   BoardDescProtocol->BoardDescSdMmcGet = MvBoardDescSdMmcGet;
   BoardDescProtocol->BoardDescXhciGet = MvBoardDescXhciGet;
