MarvellEmbeddedProcessors/uefi-marvell

Marvell: Introduce MARVELL_BOARD_DESC_PROTOCOL
This patch introduces protocol that exposes generic api to get board
description. It uses ArmadaSoCDesc library, which is implemented per
SoC family to get the SoC level description for hardware module controller.
As a first usage a GPIO information obtaining is implemented.

Change-Id: If4ba86098c04d51c1795d883091cba6b41e2f360
Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: jinghua <jinghua@marvell.com>
Reviewed-on: http://vgitil04.il.marvell.com:8080/50042
Tested-by: iSoC Platform CI <ykjenk@marvell.com>
Reviewed-by: Kostya Porotchkin <kostap@marvell.com>

OpenPlatformPkg/Platforms/Marvell/Include/Protocol/BoardDesc.h
@@ -1,0 +1,54 @@
+/*******************************************************************************
+Copyright (C) 2018 Marvell International Ltd.
+
+Marvell BSD License Option
+
+If you received this File from Marvell, you may opt to use, redistribute and/or
+modify this File under the following licensing terms.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+* Neither the name of Marvell nor the names of its contributors may be
+  used to endorse or promote products derived from this software without
+  specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*******************************************************************************/
+#ifndef __MARVELL_BOARD_DESC_PROTOCOL_H__
+#define __MARVELL_BOARD_DESC_PROTOCOL_H__
+
+#include <Library/ArmadaSoCDescLib.h>
+
+extern EFI_GUID gMarvellBoardDescProtocolGuid;
+
+typedef struct _MARVELL_BOARD_DESC_PROTOCOL MARVELL_BOARD_DESC_PROTOCOL;
+
+typedef
+EFI_STATUS
+(EFIAPI *MV_BOARD_DESC_GPIO_GET) (
+  IN MARVELL_BOARD_DESC_PROTOCOL  *This,
+  IN OUT MVHW_GPIO_DESC          **GpioDesc
+  );
+
+struct _MARVELL_BOARD_DESC_PROTOCOL {
+  MV_BOARD_DESC_GPIO_GET         BoardDescGpioGet;
+};
+
+#endif // __MARVELL_BOARD_DESC_PROTOCOL_H__

MarvellEmbeddedProcessors/edk2-open-platform

This patch introduces protocol that exposes generic API to get board
description. It uses ArmadaSoCDescLib library, which is implemented per
Together with the information obtained from ArmadaBoardDescLib
the protocol allows the drivers to get per-board information about
used hardware and settings.
As a first usage a UTMI information obtaining is implemented.

Silicon/Marvell/Include/Protocol/BoardDesc.h
@@ -1,0 +1,62 @@
+/*******************************************************************************
+Copyright (C) 2018 Marvell International Ltd.
+
+Marvell BSD License Option
+
+If you received this File from Marvell, you may opt to use, redistribute and/or
+modify this File under the following licensing terms.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+* Neither the name of Marvell nor the names of its contributors may be
+  used to endorse or promote products derived from this software without
+  specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*******************************************************************************/
+#ifndef __MARVELL_BOARD_DESC_PROTOCOL_H__
+#define __MARVELL_BOARD_DESC_PROTOCOL_H__
+
+#include <Library/ArmadaSoCDescLib.h>
+#include <Library/ArmadaBoardDescLib.h>
+
+extern EFI_GUID gMarvellBoardDescProtocolGuid;
+
+typedef struct _MARVELL_BOARD_DESC_PROTOCOL MARVELL_BOARD_DESC_PROTOCOL;
+
+typedef
+EFI_STATUS
+(EFIAPI *MV_BOARD_DESC_UTMI_GET) (
+  IN MARVELL_BOARD_DESC_PROTOCOL  *This,
+  IN OUT MV_BOARD_UTMI_DESC      **UtmiDesc
+  );
+
+typedef
+VOID
+(EFIAPI *MV_BOARD_DESC_FREE) (
+  IN VOID                         *BoardDesc
+  );
+
+struct _MARVELL_BOARD_DESC_PROTOCOL {
+  MV_BOARD_DESC_UTMI_GET         BoardDescUtmiGet;
+  MV_BOARD_DESC_FREE             BoardDescFree;
+};
+
+#endif // __MARVELL_BOARD_DESC_PROTOCOL_H__