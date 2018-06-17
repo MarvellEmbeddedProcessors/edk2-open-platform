MarvellEmbeddedProcessors/uefi-marvell

Marvell/Armada7k8k: Extend ArmadaSoCDescLib with I2C information
This patch introduces new library callback (ArmadaSoCDescI2cGet ()),
which dynamically allocates and fills MV_SOC_I2C_DESC structure with
the SoC description of I2c controllers.

Change-Id: Icad8f142cf241079d331f226f0e99e83f1f300bc
Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: Marcin Wojtas <mw@semihalf.com>
Reviewed-on: http://vgitil04.il.marvell.com:8080/52831
Tested-by: iSoC Platform CI <ykjenk@marvell.com>
Reviewed-by: Hua Jing <jinghua@marvell.com>

OpenPlatformPkg/Platforms/Marvell/Armada/Library/Armada7k8kSoCDescLib/Armada7k8kSoCDescLib.c
@@ -113,6 +113,44 @@ ArmadaSoCDescGpioGet (
 }
 
 //
+// Platform description of I2C controllers
+//
+#define MV_SOC_I2C_PER_CP_COUNT         2
+#define MV_SOC_I2C_BASE(I2c)         (0x701000 + (I2c) * 0x100)
+
+EFI_STATUS
+EFIAPI
+ArmadaSoCDescI2cGet (
+  IN OUT MV_SOC_I2C_DESC  **I2cDesc,
+  IN OUT UINT8             *DescCount
+  )
+{
+  MV_SOC_I2C_DESC *Desc;
+  UINT8 CpCount = FixedPcdGet8 (PcdMaxCpCount);
+  UINT8 Index, CpIndex, I2cIndex = 0;
+
+  Desc = AllocateZeroPool (CpCount * MV_SOC_I2C_PER_CP_COUNT *
+                           sizeof (MV_SOC_I2C_DESC));
+  if (Desc == NULL) {
+    DEBUG ((DEBUG_ERROR, "%a: Cannot allocate memory\n", __FUNCTION__));
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  for (CpIndex = 0; CpIndex < CpCount; CpIndex++) {
+    for (Index = 0; Index < MV_SOC_I2C_PER_CP_COUNT; Index++) {
+      Desc[I2cIndex].I2cBaseAddress =
+                         MV_SOC_CP_BASE (CpIndex) + MV_SOC_I2C_BASE (Index);
+      I2cIndex++;
+    }
+  }
+
+  *I2cDesc = Desc;
+  *DescCount = I2cIndex;
+
+  return EFI_SUCCESS;
+}
+
+//
 // Platform description of MDIO controllers
 //
 #define MV_SOC_MDIO_BASE(Cp)            MV_SOC_CP_BASE ((Cp)) + 0x12A200

OpenPlatformPkg/Platforms/Marvell/Include/Library/ArmadaSoCDescLib.h
@@ -53,6 +53,21 @@ ArmadaSoCDescGpioGet (
   );
 
 //
+// I2C
+//
+typedef struct {
+  UINT8 I2cId;
+  UINTN I2cBaseAddress;
+} MV_SOC_I2C_DESC;
+
+EFI_STATUS
+EFIAPI
+ArmadaSoCDescI2cGet (
+  IN OUT MV_SOC_I2C_DESC  **I2cDesc,
+  IN OUT UINT8             *DescCount
+  );
+
+//
 // MDIO
 //
 typedef struct {

MarvellEmbeddedProcessors/edk2-open-platform

Silicon/Marvell/Armada7k8k/Library/Armada7k8kSoCDescLib/Armada7k8kSoCDescLib.c
@@ -63,6 +63,37 @@ ArmadaSoCDescComPhyGet (
 
 EFI_STATUS
 EFIAPI
+ArmadaSoCDescI2cGet (
+  IN OUT MV_SOC_I2C_DESC  **I2cDesc,
+  IN OUT UINTN             *DescCount
+  )
+{
+  MV_SOC_I2C_DESC *Desc;
+  UINTN CpCount, CpIndex, Index;
+
+  CpCount = FixedPcdGet8 (PcdMaxCpCount);
+
+  *DescCount = CpCount * MV_SOC_I2C_PER_CP_COUNT;
+  Desc = AllocateZeroPool (*DescCount * sizeof (MV_SOC_I2C_DESC));
+  if (Desc == NULL) {
+    DEBUG ((DEBUG_ERROR, "%a: Cannot allocate memory\n", __FUNCTION__));
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  *I2cDesc = Desc;
+
+  for (CpIndex = 0; CpIndex < CpCount; CpIndex++) {
+    for (Index = 0; Index < MV_SOC_I2C_PER_CP_COUNT; Index++) {
+      Desc->I2cBaseAddress = MV_SOC_CP_BASE (CpIndex) + MV_SOC_I2C_BASE (Index);
+      Desc++;
+    }
+  }
+
+  return EFI_SUCCESS;
+}
+
+EFI_STATUS
+EFIAPI
 ArmadaSoCDescMdioGet (
   IN OUT MV_SOC_MDIO_DESC  **MdioDesc,
   IN OUT UINTN              *DescCount

Silicon/Marvell/Include/Library/ArmadaSoCDescLib.h
@@ -37,6 +37,21 @@ ArmadaSoCDescComPhyGet (
   );
 
 //
+// I2C
+//
+typedef struct {
+  UINTN I2cId;
+  UINTN I2cBaseAddress;
+} MV_SOC_I2C_DESC;
+
+EFI_STATUS
+EFIAPI
+ArmadaSoCDescI2cGet (
+  IN OUT MV_SOC_I2C_DESC  **I2cDesc,
+  IN OUT UINTN             *DescCount
+  );
+
+//
 // MDIO
 //
 typedef struct {