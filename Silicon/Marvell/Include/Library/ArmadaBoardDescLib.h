MarvellEmbeddedProcessors/uefi-marvell

Marvell/Library: Introduce ArmadaBoardDescLib class
This patch adds a new library class that will be helpful
to describe a per-board information, which will be
processed by BoardDesc protocol.

Together with ArmadaSoCDescLib data it will be a flexible
solution allowing to provide complete information to
the drivers, replacing faulty MvHwDescLib.h.

Initially ArmadaBoardDescLib defines per-board UTMI
PHYs information structure.

Change-Id: If464e0f27017d5479f8407de0eea30d8446aa01e
Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: Marcin Wojtas <mw@semihalf.com>
Reviewed-on: http://vgitil04.il.marvell.com:8080/52602
Tested-by: iSoC Platform CI <ykjenk@marvell.com>
Reviewed-by: Hua Jing <jinghua@marvell.com>

OpenPlatformPkg/Platforms/Marvell/Include/Library/ArmadaBoardDescLib.h
@@ -1,0 +1,27 @@
+/**
+*
+*  Copyright (C) 2018, Marvell International Ltd. and its affiliates
+*
+*  This program and the accompanying materials are licensed and made available
+*  under the terms and conditions of the BSD License which accompanies this
+*  distribution. The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+#ifndef __ARMADA_BOARD_DESC_LIB_H__
+#define __ARMADA_BOARD_DESC_LIB_H__
+
+#include <Library/ArmadaSoCDescLib.h>
+
+//
+// UTMI PHY devices per-board description
+//
+typedef struct {
+  MV_SOC_UTMI_DESC *SoC;
+  UINT8             UtmiDevCount;
+  UINT8             UtmiPortType;
+} MV_BOARD_UTMI_DESC;
+#endif /* __ARMADA_SOC_DESC_LIB_H__ */

MarvellEmbeddedProcessors/edk2-open-platform

Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>

Silicon/Marvell/Include/Library/ArmadaBoardDescLib.h
@@ -1,0 +1,27 @@
+/**
+*
+*  Copyright (C) 2018, Marvell International Ltd. and its affiliates
+*
+*  This program and the accompanying materials are licensed and made available
+*  under the terms and conditions of the BSD License which accompanies this
+*  distribution. The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+#ifndef __ARMADA_BOARD_DESC_LIB_H__
+#define __ARMADA_BOARD_DESC_LIB_H__
+
+#include <Library/ArmadaSoCDescLib.h>
+
+//
+// UTMI PHY devices per-board description
+//
+typedef struct {
+  MV_SOC_UTMI_DESC *SoC;
+  UINTN             UtmiDevCount;
+  UINTN             UtmiPortType;
+} MV_BOARD_UTMI_DESC;
+#endif /* __ARMADA_SOC_DESC_LIB_H__ */