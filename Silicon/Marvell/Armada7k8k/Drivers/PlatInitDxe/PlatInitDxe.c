5c90c05107100bbef59064e2856b1294a4660f9f
Marcin Wojtas <mw@semihalf.com>
2018-07-25 17:14:06 +0200

    Marvell/Armada7k8k: Implement PMU interrupt handling
    Generic handling of the PMU interrupts in UEFI and Linux with
    ACPI require enabling a dedicated handler in the EL3.
    Extend the PlatInitDxe with enabler code.
    
    Because for DT world the EL3 service must remain disabled,
    switch it off in the ExitBootServicesEvent. Its execution
    depends on the gEdkiiPlatformHasAcpiGuid status check in the new
    gEfiEventReadyToBootGuid routine.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Marcin Wojtas <mw@semihalf.com>

--- Silicon/Marvell/Armada7k8k/Drivers/PlatInitDxe/PlatInitDxe.c
+++ Silicon/Marvell/Armada7k8k/Drivers/PlatInitDxe/PlatInitDxe.c
@@ -12,7 +12,12 @@
 
 **/
 
+#include <Guid/EventGroup.h>
+
+#include <IndustryStandard/MvSmc.h>
+
 #include <Library/ArmadaIcuLib.h>
+#include <Library/ArmSmcLib.h>
 #include <Library/DebugLib.h>
 #include <Library/MppLib.h>
 #include <Library/MvComPhyLib.h>
@@ -21,6 +26,62 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UtmiPhyLib.h>
 
+STATIC EFI_EVENT mArmada7k8kExitBootServicesEvent;
+
+/**
+  Disable extra EL3 handling of the PMU interrupts for DT world.
+
+  @param[in]   Event                  Event structure
+  @param[in]   Context                Notification context
+
+**/
+STATIC
+VOID
+Armada7k8kExitBootServicesHandler (
+  IN EFI_EVENT  Event,
+  IN VOID      *Context
+  )
+{
+  ARM_SMC_ARGS SmcRegs = {0};
+
+  SmcRegs.Arg0 = MV_SMC_ID_PMU_IRQ_DISABLE;
+  ArmCallSmc (&SmcRegs);
+
+  return;
+}
+
+/**
+  Check if we boot with DT and trigger EBS event in such case.
+
+  @param[in]   Event                  Event structure
+  @param[in]   Context                Notification context
+
+**/
+STATIC
+VOID
+Armada7k8kOnReadyToBootHandler (
+  IN EFI_EVENT  Event,
+  IN VOID      *Context
+  )
+{
+  EFI_STATUS    Status;
+  VOID         *Interface;
+
+  Status = gBS->LocateProtocol (&gEdkiiPlatformHasAcpiGuid,
+                  NULL,
+                  (VOID **)&Interface);
+  if (EFI_ERROR (Status)) {
+    Status = gBS->CreateEvent (EVT_SIGNAL_EXIT_BOOT_SERVICES,
+                    TPL_NOTIFY,
+                    Armada7k8kExitBootServicesHandler,
+                    NULL,
+                    &mArmada7k8kExitBootServicesEvent);
+    if (EFI_ERROR (Status)) {
+      DEBUG ((DEBUG_ERROR, "%a: Fail to register EBS event\n", __FUNCTION__));
+    }
+  }
+}
+
 EFI_STATUS
 EFIAPI
 ArmadaPlatInitDxeEntryPoint (
@@ -28,7 +89,9 @@ ArmadaPlatInitDxeEntryPoint (
   IN EFI_SYSTEM_TABLE   *SystemTable
   )
 {
+  ARM_SMC_ARGS  SmcRegs = {0};
   EFI_STATUS    Status;
+  EFI_EVENT     Event;
 
   DEBUG ((DEBUG_ERROR, "\nArmada Platform Init\n\n"));
 
@@ -42,6 +105,25 @@ ArmadaPlatInitDxeEntryPoint (
   UtmiPhyInit ();
   MppInitialize ();
   ArmadaIcuInitialize ();
+
+  /*
+   * Enable EL3 PMU interrupt handler and
+   * register the ReadyToBoot event.
+   */
+  SmcRegs.Arg0 = MV_SMC_ID_PMU_IRQ_ENABLE;
+  ArmCallSmc (&SmcRegs);
+
+  Status = gBS->CreateEventEx (EVT_NOTIFY_SIGNAL,
+                  TPL_CALLBACK,
+                  Armada7k8kOnReadyToBootHandler,
+                  NULL,
+                  &gEfiEventReadyToBootGuid,
+                  &Event);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR,
+      "%a: Fail to register OnReadyToBoot event\n",
+      __FUNCTION__));
+  }
 
   return EFI_SUCCESS;
 }
